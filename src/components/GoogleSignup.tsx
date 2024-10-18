472ba87bb4cd54add219d83e73bdfc829590d0ad
sahilworkdev <sahil.malik@thewasserstoff.com>
2024-10-18 15:38:27 +0530

    jshdvfg

--- src/components/GoogleSignup.tsx
+++
@@ -1,9 +0,0 @@
-import { signIn } from "next-auth/react";
-
-export default function GoogleSignup() {
-  const handleSignIn = async () => {
-    await signIn("google", { callbackUrl: "/" });
-  };
-
-  return <button onClick={handleSignIn}>Sign In</button>;
-}
